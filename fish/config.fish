34a8a3b7dbd91d6e92e8481ff48b0973c55ab38c
Hirak Jyoti Das <dhirakjyoti11@gmail.com>
2024-09-03 22:06:28 +0530

    added tiling WM config files

---
+++ fish/config.fish
@@ -0,0 +1,6 @@
+oh-my-posh init fish --config $HOME/.poshthemes/takuya.omp.json | source
+set -g fish_greeting ""
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
