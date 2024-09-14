377fff8d307bf28f041f659fb257bfebae4c8f20
Hirak Jyoti Das <dhirakjyoti11@gmail.com>
2024-09-14 18:40:43 +0530

    not using custom colorscheme

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -14,6 +14,24 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
     -- "folke/tokyonight.nvim",
     "tanvirtin/monokai.nvim",
+
+     -- Syntax highlighting with Treesitter
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = ':TSUpdate',  -- Auto update parsers
+        config = function()
+            require'nvim-treesitter.configs'.setup {
+                ensure_installed = { "java", "lua", "bash", "python" },  -- Add your languages here
+                highlight = {
+                    enable = true,  -- Enable syntax highlighting
+                },
+                indent = {
+                    enable = true,  -- Enable Treesitter-based indentation
+                },
+            }
+        end,
+    },
+
     -- Vscode-like pictograms
 	{
 		"onsails/lspkind.nvim",
