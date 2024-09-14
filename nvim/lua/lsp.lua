8365229e5bdf8d9a43103fcdaeec521a65892bcd
Hirak Jyoti Das <dhirakjyoti11@gmail.com>
2024-09-14 16:06:48 +0530

    added lua config files and deleted init.vim

---
+++ nvim/lua/lsp.lua
@@ -0,0 +1,56 @@
+-- Setup Mason
+require('mason').setup({
+    ui = {
+        icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+        }
+    }
+})
+
+-- Ensure 'jdtls' (Java) is included in Mason-LSPConfig setup
+require('mason-lspconfig').setup({
+    ensure_installed = { 'jdtls' },  -- Add 'jdtls' for Java
+})
+
+-- Require lspconfig outside of the Mason-LSPConfig setup
+local lspconfig = require("lspconfig")
+
+-- Customized on_attach function
+local on_attach = function(client, bufnr)
+    vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+
+    local bufopts = { noremap = true, silent = true, buffer = bufnr }
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
+    vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
+    vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
+    vim.keymap.set("n", "<space>wl", function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, bufopts)
+    vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
+    vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
+    vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+    vim.keymap.set("n", "<space>f", function()
+        vim.lsp.buf.format({
+            async = true,
+            filter = function(client)
+                return client.name == "null-ls"
+            end,
+        })
+    end, bufopts)
+end
+
+-- Java LSP configuration
+lspconfig.jdtls.setup({
+    on_attach = on_attach,
+    settings = {
+        -- Custom settings for jdtls can go here
+    },
+})
+
