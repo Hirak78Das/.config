c49b91b1ce060b8d504b6bba372260c8f9033ff3
Hirak Jyoti Das <dhirakjyoti11@gmail.com>
2024-10-07 10:50:11 +0530

    added nvim environment-variable path in config.fish and changed oh-my-posh theme

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,21 @@
-oh-my-posh init fish --config $HOME/.poshthemes/takuya.omp.json | source
+oh-my-posh init fish --config $HOME/.poshthemes/avit.omp.json | source
 set -g fish_greeting ""
+
+# Set global paths
+set -gx PATH /usr/bin /bin $PATH
+
+# Android SDK paths
+set -gx ANDROID_HOME /usr/lib/android-sdk
+set -gx PATH $ANDROID_HOME/cmdline-tools/11.0/bin $ANDROID_HOME/platform-tools $ANDROID_HOME/build-tools/latest $PATH
+
+set -Ux PATH /usr/local/bin/ $PATH
+
+
+# For Java 21
+set -gx JAVA_HOME /usr/lib/jvm/java-21-openjdk-amd64/
+set -gx PATH $JAVA_HOME/bin $PATH
+
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-
