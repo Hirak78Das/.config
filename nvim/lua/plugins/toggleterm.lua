1b40d1b6d5ed2698fac3351f8cf2beac4f6e8398
Hirak Jyoti Das <dhirakjyoti11@gmail.com>
2024-10-13 20:46:07 +0530

    open terminal only in normal mode <space>t

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -8,10 +8,12 @@ return {
     config = function()
       require("toggleterm").setup({
         -- Optional custom configurations (examples below)
-        open_mapping = [[<leader>t]], -- Shortcut to toggle terminal
+        -- open_mapping = [[<leader>t]], -- Shortcut to toggle terminal
         direction = "float", -- Terminal style: 'horizontal', 'vertical', 'float', 'tab'
         size = 20, -- Size of the terminal (for horizontal/vertical splits)
       })
+      -- Set keybinding to open terminal only in Normal mode
+      vim.api.nvim_set_keymap("n", "<leader>t", ":ToggleTerm<CR>", { noremap = true, silent = true })
     end,
   },
 }
